e2b0583820378d6d2cca466488333cb01704d83a
AbdelrhmanAmin <maxhd213@gmail.com>
2021-05-01 19:50:07 +0200

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import rootReducer from './reducers/index';
+import './index.css';
 
 const initalState = {
   books: [
